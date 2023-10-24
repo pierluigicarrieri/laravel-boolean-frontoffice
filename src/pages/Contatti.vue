9176daa0044b4fb54a1fb5cffc1085c6ced0b443
riccardo-giuri <richicryst@gmail.com>
2023-10-24 19:25:20 +0200

    contatti Page almost done

--- src/pages/Contatti.vue
+++ src/pages/Contatti.vue
@@ -27,41 +27,109 @@ export default {
 
 <template>
   <div class="container">
-    <h1 class="text-center">Cocktail Bar Contatti</h1>
-    <form @submit.prevent="onClickForm">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Nome</label>
-        <input
-          type="text"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          v-model="formContact.name"
-        />
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-4">
+          <h1 class="contacts">Contattaci</h1>
+        </div>
+
+        <div class="col-8">
+          <form @submit.prevent="onClickForm">
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Nome</label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                v-model="formContact.name"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                v-model="formContact.email"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputText" class="form-label">Messaggio</label>
+              <textarea
+                type="text"
+                class="form-control"
+                id="exampleInputText"
+                rows="10"
+                v-model="formContact.message"
+              ></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary rounded-pill customBtn">Invia</button>
+          </form>
+        </div>
       </div>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          v-model="formContact.email"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputText" class="form-label">Messaggio</label>
-        <textarea
-          type="text"
-          class="form-control"
-          id="exampleInputText"
-          v-model="formContact.message"
-        ></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Invia Dati</button>
-    </form>
+    </div>
+    
+    
     <div v-if="dataSendSuccess">{{ dataSendSuccess }}</div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+input[type=text]{
+  font-size: 40px;
+  background-color: transparent;
+  border: 0;
+  border-radius: 0;
+  border-bottom: 1px solid white;
+}
+
+input[type=email]{
+  font-size: 40px;
+  background-color: transparent;
+  border: 0;
+  border-radius: 0;
+  border-bottom: 1px solid white;
+}
+
+textarea{
+  background-color: transparent;
+  border: 0;
+  border-radius: 0;
+  border-bottom: 1px solid white;
+}
+
+.form-label{
+  text-transform: uppercase;
+  font-family: 'Karla', serif;
+  font-weight: 500;
+  color: #D1B875;
+  font-size: 14px;
+}
+
+.col-4{
+  padding: 0;
+  margin: 0;
+}
+
+.customBtn{
+  background-color: #D1B875;
+  padding: 16px 32px;
+  color: black;
+  text-transform: uppercase;
+}
+
+.contacts{
+  font-family: 'Playfair Display', serif;
+  font-weight: 400;
+  color: white;
+  font-size: 56px;
+  margin-top: -5px;
+}
+
+.container-fluid {
+  margin-top: 80px;
+}
+</style>
